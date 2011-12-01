3eb8df47cd30ffe3edd1dca87c09ee6807ca80fb
Franck Michaud <franckuser16@gmail.com>
2011-12-01 11:09:00 +0100

    added TODO file, indentation changes in index.php

--- index.php
+++ index.php
@@ -3,20 +3,26 @@ include_once("conf/config.php");
 
 /* les variables menu ici */
 $list_menu = array (
-		    "accueil" 	=> "Accueil",
-		    "restore" 	=> "Effectuer une restauration",
-		    "save"	=> "Créer une nouvelle sauvegarde",
+	"accueil" 	=> "Accueil",
+	"restore" 	=> "Effectuer une restauration",
+	"save"	=> "Créer une nouvelle sauvegarde",
 	"actions_distance" 	=> "Actions à distance",
 	"adhesion_domain"	=> "Adhésion au domaine",
-		     "salles" 	=> "Gérer les salles",
+	"salles" 	=> "Gérer les salles",
 	"comptes_utilisateurs"	=> "Gérer les comptes utilisateurs",
-		"images"	=> "Gérer les images",
-		"domaines"	=> "Gestion des domaines");
+	"images"	=> "Gérer les images",
+	"domaines"	=> "Gestion des domaines");
 
 
 
 session_start();
 
+//franck: mardi 29 novembre 2011, 11:38:46 (UTC+0100)
+//
+//cet import ne sert à rien
+//functions.php définit:
+//-start_lightbox qui n'est pas utilisé dans cette page
+//-redirection qui n'est utilisé nulle part
 include_once("functions.php");
 
 if(isset($_SESSION['login']))
@@ -43,26 +49,26 @@ else
 	$menu="accueil";
 }
 
-		
-	if ($menu == "logout")
-	{
-		session_destroy();
-		$right=0;
-		$_SESSION['right']=0;
-		$login="";
-		$_SESSION['login']="";
-		$menu="accueil";
-	}
 
-	
+if ($menu == "logout")
+{
+	session_destroy();
+	$right=0;
+	$_SESSION['right']=0;
+	$login="";
+	$_SESSION['login']="";
+	$menu="accueil";
+}
+
+
 if ($menu == "access")
 {
 	include_once("acces_bdd.php");
 	//changement du menu la page access n'existe pas 
 	$menu="accueil";
 }
-		
-			
+
+
 if ($right == 0)
 {
 	$titre="Authentification";
@@ -72,7 +78,7 @@ else
 	$titre=$list_menu["$menu"];
 }
 
-	
+
 ?>
 
 
@@ -104,8 +110,8 @@ else
 <div id="menu">
 
 <?php
-	if ($right != 0)
-	{
+if ($right != 0)
+{
 ?>
 <!-- Affichage du menu-->
 <br>
@@ -132,7 +138,7 @@ else
 </ul>
 
 <?php
-	}
+}
 ?>
 </div>
 
@@ -161,9 +167,9 @@ else
 
 <div id="pied">
 <p>
-			 
+
 			Design by: <a href="http://www.styleshout.com/">styleshout | </a> <a href="http://www.iutlaroche.univ-nantes.fr">IUT La Roche/Yon | Réseau et Télécommunication </a>
-			
+
 </p>
 
 <a href="<?php echo $_SERVER['PHP_SELF']; ?>" ><img src="images/footer.jpg"></a>
